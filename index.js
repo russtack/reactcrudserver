d854ae53a66f47f7e05aa1b83e875e1bb26a9a1a
russtack <stirlingest@gmail.com>
2023-05-19 19:26:07 -0400

    desc

--- index.js
+++ index.js
@@ -26,10 +26,11 @@ app.get("/books", (req, res) => {
 });
 
 app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`,`desc`,`price`, `cover`) VALUES(?)";
+  const q =
+    "INSERT INTO books (`title`,`description`,`price`, `cover`) VALUES(?)";
   const values = [
     req.body.title,
-    req.body.desc,
+    req.body.description,
     req.body.price,
     req.body.cover,
   ];
@@ -52,10 +53,10 @@ app.delete("/books/:id", (req, res) => {
 app.put("/books/:id", (req, res) => {
   const bookId = req.params.id;
   const q =
-    "UPDATE books SET `title`=?,`desc`=?,`price`=?,`cover`=? WHERE id =?";
+    "UPDATE books SET `title`=?,`description`=?,`price`=?,`cover`=? WHERE id =?";
   const values = [
     req.body.title,
-    req.body.desc,
+    req.body.description,
     req.body.price,
     req.body.cover,
   ];
